a1a2f4f7d043031e9fac25c72334aee68d87cdb0
Greg Gunner <greg@co-foundry.co.za>
2019-09-06 09:53:05 +0200

    Adding in a new browse mixin to fndry-requests package

--- resources/packages/fndry-requests/src/index.js
+++ resources/packages/fndry-requests/src/index.js
@@ -4,6 +4,9 @@ import RequestFormInline from './components/RequestFormInline';
 import RequestForm from './components/RequestForm';
 import ConfirmButton from './components/ConfirmButton';
 
+import {HasBrowseRequest, HasFilter, HasFormRequest} from './mixins/browse';
+
+
 /**
  * FndryRequests
  * (c) 2019
@@ -21,16 +24,19 @@ Plugin.install = function (Vue, options) {
 
     /**
      * @see requestForm
+     * @return {Promise<T>}
      */
     Vue.prototype.$fndryRequestForm = makeRequestForm(Vue);
 
     /**
      * @see confirmModal
+     * @return {Promise<T>}
      */
     Vue.prototype.$fndryRequestConfirm = makeRequestConfirm(Vue);
 
     /**
      * @see requestForm
+     * @return {Promise<T>}
      */
     Vue.prototype.$fndryRequestPanel = makeRequestPanel(Vue);
 
@@ -47,7 +53,11 @@ export {
     RequestFormInline as FndryRequestFormInline,
     RequestButton as FndryRequestButton,
     RequestForm as FndryRequestForm,
-    ConfirmButton as FndryConfirmButton
+    ConfirmButton as FndryConfirmButton,
+
+    HasBrowseRequest,
+    HasFilter,
+    HasFormRequest
 }
 
 export default Plugin

e15e8957c93a833aff440e2f6cba0512105936ff
Greg Gunner <greg@co-foundry.co.za>
2019-10-04 14:52:08 +0200

    Adding a NodeId Input Type

---
+++ app/Inputs/Types/NodeId.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Foundry\System\Inputs\Types;
+
+use Foundry\Core\Inputs\Contracts\Field;
+use Foundry\Core\Inputs\Types\Contracts\Inputable;
+use Foundry\Core\Inputs\Types\NumberInputType;
+
+class NodeId extends NumberInputType implements Field {
+
+	static function input( ): Inputable {
+		return ( new static(
+			'id',
+			__( 'ID' ),
+			true )
+		)
+			->setSortable( true );
+	}
+
+}

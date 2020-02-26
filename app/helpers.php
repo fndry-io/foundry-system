86ff809efd5b9dcee24ad727cee949a40cc06c0b
Greg Gunner <greg@co-foundry.co.za>
2020-02-26 16:08:15 +0200

    Adding a theme_mix method for embedding theme based mix files

--- app/helpers.php
+++ app/helpers.php
@@ -1,1 +1,20 @@
 <?php
+
+use Foundry\System\Mix;
+
+if (! function_exists('theme_mix')) {
+    /**
+     * Get the path to a versioned Mix file.
+     *
+     * @param  string  $theme
+     * @param  string  $path
+     * @param  string  $manifestDirectory
+     * @return \Illuminate\Support\HtmlString|string
+     *
+     * @throws \Exception
+     */
+    function theme_mix($theme, $path, $manifestDirectory = '')
+    {
+        return app(Mix::class)(...func_get_args());
+    }
+}

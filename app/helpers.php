2593d0aaa014d10f98064a06795b685e09fbfca4
Greg Gunner <greg@co-foundry.co.za>
2020-05-08 21:37:29 +0200

    Getting the module mix links working

--- app/helpers.php
+++ app/helpers.php
@@ -15,6 +15,23 @@ if (! function_exists('theme_mix')) {
      */
     function theme_mix($theme, $path, $manifestDirectory = '')
     {
-        return app(Mix::class)(...func_get_args());
+        return app(Mix::class)::theme(...func_get_args());
+    }
+}
+
+if (! function_exists('module_mix')) {
+    /**
+     * Get the path to a versioned Mix file.
+     *
+     * @param  string  $theme
+     * @param  string  $path
+     * @param  string  $manifestDirectory
+     * @return \Illuminate\Support\HtmlString|string
+     *
+     * @throws \Exception
+     */
+    function module_mix($module, $path, $manifestDirectory = '')
+    {
+        return app(Mix::class)::module(...func_get_args());
     }
 }
